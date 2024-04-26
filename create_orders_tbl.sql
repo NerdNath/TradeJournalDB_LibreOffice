1aed980cd344a11d579c3a5d40080e176b5f366c
NerdNath <75958588+NerdNath@users.noreply.github.com>
2024-04-26 07:42:02 -0600

    Updates All SQL Files To V0.3
    Changes all the SQL files except for the insert files. Removes redundant columns and relationships, and reorders some columns to be compatible with the trade entry macro dialog. Expands the size of the order number column into a BIGINT with 19 characters.

--- create_orders_tbl.sql
+++ create_orders_tbl.sql
@@ -1,14 +1,14 @@
+
 CREATE TABLE "Orders" (
   trade_id INT,
   account_number INT,
-  order_number INT,
-  ticker VARCHAR(6),
+  order_number BIGINT,
   action VARCHAR(4),
   submit_date DATE,
   exe_type VARCHAR(3),
-  requested_price NUMERIC,
+  requested_price NUMERIC(10,5),
   status VARCHAR(20),
   qty_requested INT,
   PRIMARY KEY(account_number, order_number),
-  CONSTRAINT UC_order UNIQUE(account_number,order_number,ticker,action)
+  CONSTRAINT UC_order UNIQUE(trade_id,order_number)
 );
--- foreign_key_alteration.sql
+++ foreign_key_alteration.sql
@@ -1,9 +1,10 @@
-ALTER TABLE "Orders"
+
+ALTER TABLE "Orders" 
 ADD FOREIGN KEY(trade_id) REFERENCES "TradeList"(trade_id);
 
 ALTER TABLE "Executions"
-ADD FOREIGN KEY(account_number,order_number,ticker,action) 
-REFERENCES "Orders"(account_number,order_number,ticker,action);
+ADD FOREIGN KEY(account_number,order_number) 
+REFERENCES "Orders"(account_number,order_number);
 
 ALTER TABLE "Executions"
 ADD FOREIGN KEY(trade_id)
